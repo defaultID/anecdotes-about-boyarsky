5dcc8f19e4ec892406353585e6c0c9f2b6d4c8de
defaultID <grankinvyacheslav@gmail.com>
2018-12-31 05:05:29 +0500

    Разбор до like

---
+++ js/ajax/like.js
@@ -0,0 +1,24 @@
+$('.like').die('click').live("click",function() {
+    var id = $(this).attr("class").split("like");
+    id = +id[1];
+    var rel = $(this).attr("rel");
+    var URL = 'server/like.php';
+    var dataString = 'id=' + id + '&rel=' + rel;
+    $.ajax({
+        type: "POST",
+        url: URL,
+        data: dataString,
+        cache: false,
+        success: function (html) {
+            if (rel === 'Like') {
+                setcookie(id,id,10);
+                $('.' + id).attr('rel', 'Unlike').attr('class', 'doneLike like ' + id);
+                $('.amount' + id).html(html);
+            } else {
+                setcookie(id,id,-10);
+                $('.' + id).attr('rel', 'Like').attr('class', 'like ' + id);
+                $('.amount' + id).html(html);
+            }
+        }
+    })
+});
--- js/cookie.js
+++ js/cookie.js
@@ -41,9 +41,9 @@ function exit( status ) {
 
 function setcookie(a,b,c) {if(c){var d = new Date();d.setDate(d.getDate()+c);}if(a && b) document.cookie = a+'='+b+(c ? '; expires='+d.toUTCString() : '');else return false;}
 function getcookie(a) {var b = new RegExp(a+'=([^;]){1,}');var c = b.exec(document.cookie);if(c) c = c[0].split('=');else return false;return c[1] ? c[1] : false;}
-setcookie("block","yes",10) // cтавим кук (10 - число действующих дней)
+setcookie("block","yes",10); // cтавим кук (10 - число действующих дней)
 var block = getcookie("block");
-if (block != "yes") {
+if (block !== "yes") {
     alert('Для корректной работы сайта необходимо включить cookies');
     exit();
 }
